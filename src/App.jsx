27f2df313a02f85807f5cedbd096e06bef3bd9a2
omar09ku-commits <omar09ku@gmail.com>
2026-03-28 20:11:13 +0500

    Create KhaiFinance game component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const KhaiFinanceGame = () => {
+    return (
+        <div>
+            <h1>KhaiFinance Game</h1>
+            <p>Welcome to the KhaiFinance game!</p>
+        </div>
+    );
+};
+
+export default KhaiFinanceGame;
